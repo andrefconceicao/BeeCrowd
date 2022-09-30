d3dd7086d72e3249836ea424b918677dc6301d3f
André Filipe <daboiul@gmail.com>
2022-09-30 17:04:23 -0300

    1181 e 1182

--- 1_Iniciante/11xx/1182.py
+++ 1_Iniciante/11xx/1182.py
@@ -1,27 +1,27 @@
-l = int(input())
+c = int(input())
 t = input()
+matriz = []
 
 coluna = 0
 linha = 0
 
-matriz = []
-
 while(coluna < 12):
     linhaV = []
-    linha = 0
     while(linha < 12):
-        linhaV.append(float(input()))
+        valor = float(input())
+        linhaV.append(valor)
         linha += 1
-
+    linha = 0
     matriz.append(linhaV)
     coluna += 1
 
-contador = 0
+coluna = 0
+linha = 0
 soma = 0
 
-while(contador < 12):
-    soma += float(matriz[l][contador])
-    contador += 1
+while(coluna<12):
+    soma += matriz[coluna][c]
+    coluna += 1
     
 if(t == 'S'):
     print("%.1f" % soma)
